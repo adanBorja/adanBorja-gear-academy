b5e679a42665256d6455919528a1f5e856f99162
Adan <adan.borja.cadena.83@gmail.com>
2024-01-30 13:06:49 -0600

    fist version of tamagotchi-nft

--- contracts/03-tamagotchi-nft/io/src/lib.rs
+++ contracts/03-tamagotchi-nft/io/src/lib.rs
@@ -1,14 +1,67 @@
 #![no_std]
 
-use gmeta::Metadata;
-use gstd::prelude::*;
+use codec::{Decode, Encode};
+use gmeta::{In, InOut, Metadata, Out};
+
+use gstd::{exec, prelude::*, ActorId};
+use scale_info::TypeInfo;
 
 #[derive(Default, Encode, Decode, TypeInfo)]
 #[codec(crate = gstd::codec)]
 #[scale_info(crate = gstd::scale_info)]
 pub struct Tamagotchi {
     // TODO: 0️⃣ Copy fields from previous lesson and push changes to the master branch
+    pub name: String,
+    pub date_of_birth: u64,
+    pub owner: ActorId,
+    pub fed: u64,
+    pub fed_block: u64,
+    pub entertained: u64,
+    pub entertained_block: u64,
+    pub slept: u64,
+    pub slept_block: u64,
     // TODO: 1️⃣ Add new fields
+    pub approved_account: Option<ActorId>,
+}
+
+const HUNGER_PER_BLOCK:u64 = 1;          // - how much Tamagotchi becomes hungry for the block (becomes less fed); 
+const BOREDOM_PER_BLOCK:u64 = 2;         // - how bored Tamagotchi gets per block (becomes less entertained); 
+const ENERGY_PER_BLOCK:u64 = 2;          // - how much Tamagotchi loses energy per block (becomes less slept); 
+const FILL_PER_FEED:u64 = 1000;          // - how much Tamagotchi becomes full during feeding; 
+const FILL_PER_ENTERTAINMENT:u64 = 1000; // - how much Tamagotchi becomes happy during entertainment; 
+const FILL_PER_SLEEP:u64 = 1000;         // - how much energy Tamagotchi gets per sleep.
+
+impl Tamagotchi {
+    pub fn feed(&mut self){
+        let hungry:u64 = HUNGER_PER_BLOCK * ((exec::block_timestamp() - self.fed_block) / 10000); //currentry hungry
+        if hungry > FILL_PER_FEED {
+            self.fed = FILL_PER_FEED;
+        } 
+        else {
+            self.fed += FILL_PER_FEED - hungry;
+        }
+        self.fed_block=exec::block_timestamp();
+    }     
+    pub fn play(&mut self){
+        let boredom_tmp:u64 = BOREDOM_PER_BLOCK * ((exec::block_timestamp() - self.entertained_block) / 1000);
+        if boredom_tmp > FILL_PER_ENTERTAINMENT{
+            self.entertained = FILL_PER_ENTERTAINMENT;
+        }
+        else{
+            self.entertained += FILL_PER_ENTERTAINMENT - boredom_tmp;
+        }
+        self.entertained_block=exec::block_timestamp();
+    }
+    pub fn get_sleep(&mut self){
+        let energy_lost:u64 = ENERGY_PER_BLOCK * ((exec::block_timestamp() - self.slept_block)/1000);
+        if energy_lost > FILL_PER_SLEEP{
+            self.slept = FILL_PER_SLEEP;
+        }   
+        else{
+            self.slept += FILL_PER_SLEEP - energy_lost;
+        }    
+        self.slept_block=exec::block_timestamp();
+    }
 }
 
 #[derive(Encode, Decode, TypeInfo)]
@@ -16,7 +69,15 @@ pub struct Tamagotchi {
 #[scale_info(crate = gstd::scale_info)]
 pub enum TmgAction {
     // TODO: 0️⃣ Copy actions from previous lesson and push changes to the master branch
+    Name,
+    Age,
+    Feed,
+    Entertain,
+    Sleep,
     // TODO: 2️⃣ Add new actions
+    Transfer(ActorId),
+    Approve(ActorId),
+    RevokeApproval,
 }
 
 #[derive(Encode, Decode, TypeInfo)]
@@ -24,17 +85,25 @@ pub enum TmgAction {
 #[scale_info(crate = gstd::scale_info)]
 pub enum TmgEvent {
     // TODO: 0️⃣ Copy events from previous lesson and push changes to the master branch
+    Name(String),
+    Age(u64),
+    Fed,
+    Entertained,
+    Slept,
     // TODO: 3️⃣ Add new events
+    Transferred(ActorId),
+    Approved(ActorId),
+    ApprovalRevoked,
 }
 
 pub struct ProgramMetadata;
 
 // TODO: 0️⃣ Copy `Metadata` from the first lesson and push changes to the master branch
 impl Metadata for ProgramMetadata {
-    type Init = ();
-    type Handle = ();
-    type State = ();
+    type Init = In<String>;
+    type Handle = InOut<TmgAction, TmgEvent>;
     type Reply = ();
     type Others = ();
     type Signal = ();
+    type State = Out<Tamagotchi>;
 }
--- contracts/03-tamagotchi-nft/src/lib.rs
+++ contracts/03-tamagotchi-nft/src/lib.rs
@@ -1,19 +1,106 @@
 #![no_std]
 
 #[allow(unused_imports)]
-use gstd::prelude::*;
+use gstd::{msg,prelude::*,debug,exec,ActorId};
+use tamagotchi_nft_io::*;
 
-#[no_mangle]
-extern fn init() {
-    // TODO: 0️⃣ Copy the `init` function from the previous lesson and push changes to the master branch
-}
+static mut TAMAGOTCHI: Option<Tamagotchi> = None;
 
 #[no_mangle]
 extern fn handle() {
     // TODO: 0️⃣ Copy the `handle` function from the previous lesson and push changes to the master branch
+    let input_message:TmgAction=msg::load()
+        .expect("Error in loading TmgAction");
+    let tamagotchi = unsafe { 
+        TAMAGOTCHI.get_or_insert(Default::default()) 
+    };
+    match input_message{
+        TmgAction::Name=>{
+            msg::reply(TmgEvent::Name(tamagotchi.name.clone()),0)
+                .expect("Error in sending Name");
+        }
+        TmgAction::Age=>{
+            msg::reply(TmgEvent::Age(tamagotchi.date_of_birth), 0)
+                .expect("Error in sendig Age");
+        }
+        TmgAction::Feed=>{
+            tamagotchi.feed();
+            msg::reply(TmgEvent::Fed,0)
+                .expect("Error with update fed");
+        }
+        TmgAction::Entertain=>{
+            tamagotchi.play();
+            msg::reply(TmgEvent::Entertained, 0)
+                .expect("Error update Entertan");
+        }
+        TmgAction::Sleep=>{
+            tamagotchi.get_sleep();
+            msg::reply(TmgEvent::Slept, 0)
+                .expect("Error update Sleep");
+        }
+        TmgAction::Transfer(actor_id)=>{
+            let source_id = msg::source();
+            let mut owner_trasfered = false;
+            if tamagotchi.owner == source_id{
+                tamagotchi.owner = actor_id;
+                owner_trasfered = true;
+            }
+            if tamagotchi.approved_account == Some(source_id){
+                tamagotchi.owner=actor_id;
+                owner_trasfered=true;
+            }
+            if owner_trasfered{
+                msg::reply(TmgEvent::Transferred(actor_id), 0)
+                .expect("Error in trasnfer reply tamagotchi");
+            }
+        }
+        TmgAction::Approve(actor_id)=>{
+            let soruce_id = msg::source();
+            if tamagotchi.owner == soruce_id {
+                tamagotchi.approved_account = Some(actor_id);
+                msg::reply(TmgEvent::Approved(actor_id),0)
+                .expect("Error en reply to Approve");
+            }
+        }
+        TmgAction::RevokeApproval=>{
+            let source_id = msg::source();
+            if tamagotchi.owner == source_id {
+                tamagotchi.approved_account=None;
+                msg::reply(TmgEvent::ApprovalRevoked,0)
+                .expect("Error in reply RevokeApproval");
+            }
+        }
+    }
 }
 
 #[no_mangle]
 extern fn state() {
     // TODO: 0️⃣ Copy the `handle` function from the previous lesson and push changes to the master branch
+    let tamagotchi = unsafe { TAMAGOTCHI.take().expect("Unexpected error in taking state") };
+    msg::reply(tamagotchi, 0).expect("Failed to share state");
+}
+
+#[no_mangle]
+extern fn init() {
+    // TODO: 0️⃣ Copy the `init` function from the previous lesson and push changes to the master branch
+    let tamagochi_name:String=msg::load()
+        .expect("Can't decode an init message");
+    let current_birth = exec::block_timestamp();
+    let tamagotchi = Tamagotchi{
+        name: tamagochi_name,
+        date_of_birth: current_birth,
+        owner:msg::source(),
+        fed:50,
+        fed_block: current_birth,
+        entertained: 50,
+        entertained_block: current_birth,
+        slept: 50,
+        slept_block:current_birth,
+        approved_account: None,
+    };
+    unsafe{
+        TAMAGOTCHI=Some(tamagotchi);
+    }
+    msg::reply("Successfull initialize of tamagochi", 0)
+        .expect("Can't send reply");
 }
